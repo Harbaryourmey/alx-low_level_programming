cd2c762331f434d8b94407d7ec32c7941258e385
Harbaryourmey <adebolaabayomi07@gmail.com>
2022-09-13 22:59:23 +0000

    script

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <main.h>
+/**
+* main - Entry point
+*
+* Description:
+*
+* Return: 0 on Success.
+*/
+int main(void)
+{
+char p[] = "_putchar";
+int c;
+for (c = 0; c < 8; c++)
+{
+_putchar(p[c]);
+}
+_putchar("\n");
+return (0);
+}
