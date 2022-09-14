f8884dadbed466b5721b9be9ddd8969a68ad5abc
Harbaryourmey <adebolaabayomi07@gmail.com>
2022-09-14 02:38:08 +0000

    script

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 *
 * _isalpha - To checks for alphabetic character.
 * @c: character to test.
-* Return: 1 if character is English alphabet, and zero if otherwise..
+* Return: 1 if character is English alphabet, and 0 if otherwise..
 */
 int _isalpha(int c)
 {
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,7 @@ int print_last_digit(int n)
 {
 int l = n % 10;
 if (n < 0)
-n = n * -1;
+l = l * -1;
 _putchar(l + '0');
 return (l);
 }
